4769d980ea51f07c6c4db7882c31cd10e333ba3a
MoDeeJr <dinmorrison567@gmail.com>
2023-05-26 11:13:59 -0700

    testing.

--- test2/src/test2/Main.java
+++ test2/src/test2/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Editor constructed.");
 		Editor e = new Editor();
-		 
 	}
 }
